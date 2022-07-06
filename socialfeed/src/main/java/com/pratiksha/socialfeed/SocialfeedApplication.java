7feaaf66a59b26f56366b329446c683d8f8d8bb5
Pratiksha-angularminds <pratiksha@angularminds.in>
2022-07-06 11:53:34 +0530

    social feed

---
+++ socialfeed/src/main/java/com/pratiksha/socialfeed/SocialfeedApplication.java
@@ -0,0 +1,14 @@
+package com.pratiksha.socialfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialfeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialfeedApplication.class, args);
+		System.out.println("--------------started------------------");
+	}
+
+}
